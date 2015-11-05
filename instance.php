6e0e9f23b5dac5695389976414508f255118e01f
Derek Herman <derek@valendesigns.com>
2015-11-04 20:16:31 -0800

    General clean up

---
+++ instance.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Instantiates Customize Snapshots
+ *
+ * @package CustomizeSnapshot
+ */
+
+namespace CustomizeSnapshots;
+
+global $customize_snapshots_plugin;
+
+require_once __DIR__ . '/php/class-plugin-base.php';
+require_once __DIR__ . '/php/class-plugin.php';
+
+$customize_snapshots_plugin = new Plugin();
+
+/**
+ * Customize Snapshots Plugin Instance
+ *
+ * @return Plugin
+ */
+function get_plugin_instance() {
+	global $customize_snapshots_plugin;
+	return $customize_snapshots_plugin;
+}
