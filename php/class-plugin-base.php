05eeb2498ef0cdd3eb22fb7834563e6ff688554d
Weston Ruter <weston@xwp.co>
2016-06-02 15:03:55 -0700

    Fix unit tests by incorporating latest changes from foo-bar
    See https://github.com/xwp/wp-foo-bar/pull/13

--- php/class-plugin-base.php
+++ php/class-plugin-base.php
@@ -1,46 +1,101 @@
 <?php
+/**
+ * Class Plugin_Base
+ *
+ * @package CustomizeSnapshots
+ */
 
 namespace CustomizeSnapshots;
 
+/**
+ * Class Plugin_Base
+ *
+ * @package CustomizeSnapshots
+ */
 abstract class Plugin_Base {
 
 	/**
+	 * Plugin config.
+	 *
 	 * @var array
 	 */
 	public $config = array();
 
 	/**
+	 * Plugin slug.
+	 *
 	 * @var string
 	 */
 	public $slug;
 
 	/**
+	 * Plugin directory path.
+	 *
 	 * @var string
 	 */
 	public $dir_path;
 
 	/**
+	 * Plugin directory URL.
+	 *
 	 * @var string
 	 */
 	public $dir_url;
 
 	/**
+	 * Directory in plugin containing autoloaded classes.
+	 *
 	 * @var string
 	 */
 	protected $autoload_class_dir = 'php';
 
-	function __construct() {
+	/**
+	 * Autoload matches cache.
+	 *
+	 * @var array
+	 */
+	protected $autoload_matches_cache = array();
+
+	/**
+	 * All DocBlock added hooks.
+	 *
+	 * @var array
+	 */
+	protected $added_doc_hooks = array();
+
+	/**
+	 * Required instead of a static variable inside the add_doc_hooks method
+	 * for the sake of unit testing.
+	 *
+	 * @var array
+	 */
+	protected $_called_doc_hooks = array();
+
+	/**
+	 * Plugin_Base constructor.
+	 */
+	public function __construct() {
 		$location = $this->locate_plugin();
 		$this->slug = $location['dir_basename'];
 		$this->dir_path = $location['dir_path'];
 		$this->dir_url = $location['dir_url'];
 		spl_autoload_register( array( $this, 'autoload' ) );
+		$this->add_doc_hooks();
 	}
 
 	/**
+	 * Plugin_Base destructor.
+	 */
+	function __destruct() {
+		$this->remove_doc_hooks();
+	}
+
+	/**
+	 * Get reflection object for this class.
+	 *
 	 * @return \ReflectionObject
 	 */
-	function get_object_reflection() {
+	public function get_object_reflection() {
 		static $reflection;
 		if ( empty( $reflection ) ) {
 			$reflection = new \ReflectionObject( $this );
@@ -48,15 +103,13 @@ abstract class Plugin_Base {
 		return $reflection;
 	}
 
-	protected $autoload_matches_cache = array();
-
 	/**
 	 * Autoload for classes that are in the same namespace as $this.
 	 *
-	 * @param  string $class
+	 * @param string $class Class name.
 	 * @return void
 	 */
-	function autoload( $class ) {
+	public function autoload( $class ) {
 		if ( ! isset( $this->autoload_matches_cache[ $class ] ) ) {
 			if ( ! preg_match( '/^(?P<namespace>.+)\\\\(?P<class>[^\\\\]+)$/', $class, $matches ) ) {
 				$matches = false;
@@ -87,33 +140,53 @@ abstract class Plugin_Base {
 	 * Version of plugin_dir_url() which works for plugins installed in the plugins directory,
 	 * and for plugins bundled with themes.
 	 *
-	 * @throws \Exception
+	 * @throws \Exception If the plugin is not located in the expected location.
 	 * @return array
 	 */
 	public function locate_plugin() {
-		$reflection = new \ReflectionObject( $this );
-		$file_name = $reflection->getFileName();
+		$file_name = $this->get_object_reflection()->getFileName();
 		if ( '/' !== \DIRECTORY_SEPARATOR ) {
-			$file_name = str_replace( \DIRECTORY_SEPARATOR, '/', $file_name ); // Windows compat
+			$file_name = str_replace( \DIRECTORY_SEPARATOR, '/', $file_name ); // Windows compat.
 		}
 		$plugin_dir = preg_replace( '#(.*plugins[^/]*/[^/]+)(/.*)?#', '$1', $file_name, 1, $count );
 		if ( 0 === $count ) {
 			throw new \Exception( "Class not located within a directory tree containing 'plugins': $file_name" );
 		}
 
-		// Make sure that we can reliably get the relative path inside of the content directory
-		$content_dir = trailingslashit( WP_CONTENT_DIR );
-		if ( '/' !== \DIRECTORY_SEPARATOR ) {
-			$content_dir = str_replace( \DIRECTORY_SEPARATOR, '/', $content_dir ); // Windows compat
+		// Make sure that we can reliably get the relative path inside of the content directory.
+		$plugin_path = $this->relative_path( $plugin_dir, 'wp-content', \DIRECTORY_SEPARATOR );
+		if ( '' === $plugin_path ) {
+			throw new \Exception( 'Plugin dir is not inside of the `wp-content` directory' );
 		}
-		if ( 0 !== strpos( $plugin_dir, $content_dir ) ) {
-			throw new \Exception( 'Plugin dir is not inside of WP_CONTENT_DIR' );
-		}
-		$content_sub_path = substr( $plugin_dir, strlen( $content_dir ) );
-		$dir_url = content_url( trailingslashit( $content_sub_path ) );
+
+		$dir_url = content_url( trailingslashit( $plugin_path ) );
 		$dir_path = $plugin_dir;
 		$dir_basename = basename( $plugin_dir );
 		return compact( 'dir_url', 'dir_path', 'dir_basename' );
+	}
+
+	/**
+	 * Relative Path
+	 *
+	 * Returns a relative path from a specified starting position of a full path
+	 *
+	 * @param string $path The full path to start with.
+	 * @param string $start The directory after which to start creating the relative path.
+	 * @param string $sep The directory seperator.
+	 *
+	 * @return string
+	 */
+	public function relative_path( $path, $start, $sep ) {
+		$path = explode( $sep, untrailingslashit( $path ) );
+		if ( count( $path ) > 0 ) {
+			foreach ( $path as $p ) {
+				array_shift( $path );
+				if ( $p === $start ) {
+					break;
+				}
+			}
+		}
+		return implode( $sep, $path );
 	}
 
 	/**
@@ -128,12 +201,103 @@ abstract class Plugin_Base {
 	/**
 	 * Call trigger_error() if not on VIP production.
 	 *
-	 * @param string $message
-	 * @param int $code
+	 * @param string $message Warning message.
+	 * @param int    $code    Warning code.
 	 */
 	public function trigger_warning( $message, $code = \E_USER_WARNING ) {
 		if ( ! $this->is_wpcom_vip_prod() ) {
 			trigger_error( esc_html( get_class( $this ) . ': ' . $message ), $code );
 		}
+	}
+
+	/**
+	 * Hooks a function on to a specific filter.
+	 *
+	 * @param string $name     The hook name.
+	 * @param array  $callback The class object and method.
+	 * @param array  $args     An array with priority and arg_count.
+	 *
+	 * @return mixed
+	 */
+	public function add_filter( $name, $callback, $args = array( 'priority' => 10, 'arg_count' => PHP_INT_MAX ) ) {
+		return $this->_add_hook( 'filter', $name, $callback, $args );
+	}
+
+	/**
+	 * Hooks a function on to a specific action.
+	 *
+	 * @param string $name     The hook name.
+	 * @param array  $callback The class object and method.
+	 * @param array  $args     An array with priority and arg_count.
+	 *
+	 * @return mixed
+	 */
+	public function add_action( $name, $callback, $args = array( 'priority' => 10, 'arg_count' => PHP_INT_MAX ) ) {
+		return $this->_add_hook( 'action', $name, $callback, $args );
+	}
+
+	/**
+	 * Hooks a function on to a specific action/filter.
+	 *
+	 * @param string $type     The hook type. Options are action/filter.
+	 * @param string $name     The hook name.
+	 * @param array  $callback The class object and method.
+	 * @param array  $args     An array with priority and arg_count.
+	 *
+	 * @return mixed
+	 */
+	protected function _add_hook( $type, $name, $callback, $args = array() ) {
+		$priority = isset( $args['priority'] ) ? $args['priority'] : 10;
+		$arg_count = isset( $args['arg_count'] ) ? $args['arg_count'] : PHP_INT_MAX;
+		$fn = sprintf( '\add_%s', $type );
+		$retval = \call_user_func( $fn, $name, $callback, $priority, $arg_count );
+		$this->added_doc_hooks[] = compact( 'type', 'name', 'callback', 'priority' );
+		return $retval;
+	}
+
+	/**
+	 * Add actions/filters from the methods of a class based on DocBlocks.
+	 *
+	 * @param object $object The class object.
+	 */
+	public function add_doc_hooks( $object = null ) {
+		if ( is_null( $object ) ) {
+			$object = $this;
+		}
+		$class_name = get_class( $object );
+		if ( isset( $this->_called_doc_hooks[ $class_name ] ) ) {
+			$notice = sprintf( 'The add_doc_hooks method was already called on %s. Note that the Plugin_Base constructor automatically calls this method.', $class_name );
+			if ( ! $this->is_wpcom_vip_prod() ) {
+				trigger_error( esc_html( $notice ), E_USER_NOTICE );
+			}
+			return;
+		}
+		$this->_called_doc_hooks[ $class_name ] = true;
+
+		$reflector = new \ReflectionObject( $object );
+		foreach ( $reflector->getMethods() as $method ) {
+			$doc = $method->getDocComment();
+			$arg_count = $method->getNumberOfParameters();
+			if ( preg_match_all( '#\* @(?P<type>filter|action)\s+(?P<name>[a-z0-9\-\._]+)(?:,\s+(?P<priority>\d+))?#', $doc, $matches, PREG_SET_ORDER ) ) {
+				foreach ( $matches as $match ) {
+					$type = $match['type'];
+					$name = $match['name'];
+					$priority = empty( $match['priority'] ) ? 10 : intval( $match['priority'] );
+					$callback = array( $object, $method->getName() );
+					call_user_func( array( $this, "add_{$type}" ), $name, $callback, compact( 'priority', 'arg_count' ) );
+				}
+			}
+		}
+	}
+
+	/**
+	 * Removes the added DocBlock hooks.
+	 */
+	public function remove_doc_hooks() {
+		foreach ( $this->added_doc_hooks as $added_hook ) {
+			$fn = sprintf( 'remove_%s', $added_hook['type'] );
+			call_user_func( $fn, $added_hook['name'], $added_hook['callback'], $added_hook['priority'] );
+		}
+		$this->_called_doc_hooks = array();
 	}
 }
--- tests/php/test-class-plugin-base.php
+++ tests/php/test-class-plugin-base.php
@@ -1,23 +1,40 @@
 <?php
+/**
+ * Tests for Plugin_Base.
+ *
+ * @package CustomizeSnapshots
+ */
 
 namespace CustomizeSnapshots;
 
+/**
+ * Tests for Plugin_Base.
+ */
 class Test_Plugin_Base extends \WP_UnitTestCase {
 
 	/**
+	 * Plugin instance.
+	 *
 	 * @var Plugin
 	 */
 	public $plugin;
 
-	function setUp() {
+	/**
+	 * Setup.
+	 *
+	 * @inheritdoc
+	 */
+	public function setUp() {
 		parent::setUp();
 		$this->plugin = get_plugin_instance();
 	}
 
 	/**
+	 * Test locate_plugin.
+	 *
 	 * @see Plugin_Base::locate_plugin()
 	 */
-	function test_locate_plugin() {
+	public function test_locate_plugin() {
 		$location = $this->plugin->locate_plugin();
 		$this->assertEquals( 'customize-snapshots', $location['dir_basename'] );
 		$this->assertContains( 'plugins/customize-snapshots', $location['dir_path'] );
@@ -25,22 +42,36 @@ class Test_Plugin_Base extends \WP_UnitTestCase {
 	}
 
 	/**
+	 * Test relative_path.
+	 *
+	 * @see Plugin_Base::relative_path()
+	 */
+	public function test_relative_path() {
+		$this->assertEquals( 'plugins/customize-snapshots', $this->plugin->relative_path( '/srv/www/wordpress-develop/src/wp-content/plugins/customize-snapshots', 'wp-content', '/' ) );
+		$this->assertEquals( 'themes/twentysixteen/plugins/customize-snapshots', $this->plugin->relative_path( '/srv/www/wordpress-develop/src/wp-content/themes/twentysixteen/plugins/customize-snapshots', 'wp-content', '/' ) );
+	}
+
+	/**
+	 * Tests for trigger_warning().
+	 *
 	 * @see Plugin_Base::trigger_warning()
 	 */
-	function test_trigger_warning() {
+	public function test_trigger_warning() {
 		$obj = $this;
-		set_error_handler( function ( $errno, $errstr ) use( $obj ) {
+		set_error_handler( function ( $errno, $errstr ) use ( $obj ) {
 			$obj->assertEquals( 'CustomizeSnapshots\Plugin: Param is 0!', $errstr );
 			$obj->assertEquals( \E_USER_WARNING, $errno );
-    } );
+		} );
 		$this->plugin->trigger_warning( 'Param is 0!', \E_USER_WARNING );
 		restore_error_handler();
 	}
 
 	/**
+	 * Test is_wpcom_vip_prod().
+	 *
 	 * @see Plugin_Base::is_wpcom_vip_prod()
 	 */
-	function test_is_wpcom_vip_prod() {
+	public function test_is_wpcom_vip_prod() {
 		if ( ! defined( 'WPCOM_IS_VIP_ENV' ) ) {
 			$this->assertFalse( $this->plugin->is_wpcom_vip_prod() );
 			define( 'WPCOM_IS_VIP_ENV', true );
