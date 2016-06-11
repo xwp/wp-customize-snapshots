5e72fb3a962b74c1fcc8b0fbc974ea0ef2c136c7
Derek Herman <derek@valendesigns.com>
2016-06-10 19:47:44 -0700

    Load actions and filters in Plugin with Plugin_Base::add_doc_hooks()

--- php/class-plugin.php
+++ php/class-plugin.php
@@ -20,34 +20,23 @@ class Plugin extends Plugin_Base {
 	public $customize_snapshot_manager;
 
 	/**
-	 * Class constructor.
-	 */
-	public function __construct() {
-		parent::__construct();
-
-		/*
-		 * Priority is 8 because \CustomizeWidgetsPlus\Widget_Posts::init() happens
-		 * at priority 9, and this calls \CustomizeWidgetsPlus\Widget_Posts::add_customize_hooks().
-		 * So we need priority 8 so that the Customizer will be initialized before Widget Posts
-		 * initializes.
-		 */
-		$priority = 8;
-		add_action( 'after_setup_theme', array( $this, 'init' ), $priority );
-	}
-
-	/**
 	 * Initiate the plugin resources.
+	 *
+	 * Priority is 8 because \CustomizeWidgetsPlus\Widget_Posts::init() happens
+	 * at priority 9, and this calls \CustomizeWidgetsPlus\Widget_Posts::add_customize_hooks().
+	 * So we need priority 8 so that the Customizer will be initialized before Widget Posts
+	 * initializes.
+	 *
+	 * @action after_setup_theme, 8
 	 */
 	public function init() {
-		add_action( 'wp_default_scripts', array( $this, 'register_scripts' ), 11 );
-		add_action( 'wp_default_styles', array( $this, 'register_styles' ), 11 );
-		add_action( 'user_has_cap', array( $this, 'filter_user_has_cap' ), 10, 2 );
-
 		$this->customize_snapshot_manager = new Customize_Snapshot_Manager( $this );
 	}
 
 	/**
 	 * Register scripts.
+	 *
+	 * @action wp_default_scripts, 11
 	 *
 	 * @param \WP_Scripts $wp_scripts Instance of \WP_Scripts.
 	 */
@@ -61,6 +50,8 @@ class Plugin extends Plugin_Base {
 	/**
 	 * Register styles.
 	 *
+	 * @action wp_default_styles, 11
+	 *
 	 * @param \WP_Styles $wp_styles Instance of \WP_Styles.
 	 */
 	public function register_styles( \WP_Styles $wp_styles ) {
@@ -72,6 +63,8 @@ class Plugin extends Plugin_Base {
 
 	/**
 	 * Add the customize_publish capability to users who can edit_theme_options by default.
+	 *
+	 * @filter user_has_cap
 	 *
 	 * @param array $allcaps An array of all the user's capabilities.
 	 * @param array $caps    Actual capabilities for meta capability.
